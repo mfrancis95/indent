13ced581cf7b669778e58e2fd5b62c5f07d1be0e
mfrancis95 <mikefrancis95@gmail.com>
2016-01-29 19:02:21 -0500

    Improve regex matching

--- reindent.py
+++ reindent.py
@@ -7,7 +7,7 @@ parser.add_argument("spaces", type = int)
 parser.add_argument("to", type = int)
 args = parser.parse_args()
 
-regex = "^" + " " * args.spaces + r"\b"
+regex = "^(" + " " * args.spaces + r")(?=\S)"
 substitute = " " * args.to
 
 for line in map(lambda line: sub(regex, substitute, line), sys.stdin):
