f5b1ed4334c4fff278efecd1123c1188142c07aa
mfrancis95 <mikefrancis95@gmail.com>
2016-03-18 23:50:32 -0400

    Add --file argument

--- reindent.py
+++ reindent.py
@@ -3,6 +3,7 @@ from re import sub
 import sys
 
 parser = ArgumentParser("reindent")
+parser.add_argument("-f", "--file")
 parser.add_argument("spaces", type = int)
 parser.add_argument("to", type = int)
 args = parser.parse_args()
@@ -10,5 +11,13 @@ args = parser.parse_args()
 regex = "^(" + " " * args.spaces + r")(?=\S)"
 substitute = " " * args.to
 
-for line in map(lambda line: sub(regex, substitute, line), sys.stdin):
-    sys.stdout.write(line)
+def reindent(file):
+    for line in map(lambda line: sub(regex, substitute, line), file):
+        sys.stdout.write(line)
+
+file = args.file
+if file:
+    with open(file) as f:
+        reindent(f)
+else:
+    reindent(sys.stdin)
